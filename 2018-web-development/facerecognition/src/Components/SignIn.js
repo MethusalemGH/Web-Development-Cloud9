64ea29627d9590076cbd28994cb86e646e4a6b9f
Danilo Lax <danilolax@live.com>
2018-04-01 19:49:56 +0000

    server API and connection to front end

--- 2018-web-development/facerecognition/src/Components/SignIn.js
+++ 2018-web-development/facerecognition/src/Components/SignIn.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import fetch from 'isomorphic-fetch';
 
 const styleForm = 'f5 center w-90 w-75-m w-60-ns br3 shadow-5';
 const styleLabel = 'db fw6 lh-copy f6';
@@ -6,6 +7,15 @@ const styleInput = 'pa2 input-reset ba bg-transparent hover-bg-white hover-black
 const styleSignIn = 'b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib';
 
 class SignIn extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      signInEmail: '',
+      signInPassword: ''
+    };
+  }
+
   render = () => {
     return (
       <div className={styleForm} style={{backgroundColor:'rgb(112, 176, 255)'}}>
@@ -15,15 +25,15 @@ class SignIn extends React.Component {
               <legend className={'f3 fw6 ph0 mh0'}>Sign In</legend>
               <div className={'mt3'}>
                 <label className={styleLabel} htmlFor='email-address'>Email</label>
-                <input className={styleInput} type='email' name='email-address'  id='email-address' />
+                <input className={styleInput} type='email' name='email-address'  id='email-address' onChange={this.onEmailChange} />
               </div>
               <div className={'mv3'}>
                 <label className={styleLabel} htmlFor='password'>Password</label>
-                <input className={styleInput} type='password' name='password'  id='password' />
+                <input className={styleInput} type='password' name='password'  id='password' onChange={this.onPasswordChange} />
               </div>
             </fieldset>
             <div className={''}>
-              <input className={styleSignIn} type='button' value='Sign In' onClick={() => this.props.onRouteChange('home')} />
+              <input className={styleSignIn} type='button' value='Sign In' onClick={this.onSubmitSignIn} />
             </div>
             <div className={'lh-copy mt2'}>
               <span className={'f6 link dim black db pointer w-20 dib'} onClick={() =>this.props.onRouteChange('register')}>Register</span>
@@ -32,6 +42,34 @@ class SignIn extends React.Component {
         </main>
       </div>
     );
+  }
+
+  onEmailChange = (event) => {
+    this.setState({ signInEmail: event.target.value });
+  }
+
+  onPasswordChange = (event) => {
+    this.setState({ signInPassword: event.target.value });
+  }
+
+  onSubmitSignIn = () => {
+    const connection = this.props.connection;
+    fetch(`${connection.url}:${connection.port}/signin`, {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          email: this.state.signInEmail,
+          password: this.state.signInPassword
+        })
+      })
+      .then((response) => response.json())
+      .then((user) => {
+        if (user.id > 0) {
+          console.log(user);
+          this.props.loadUser(user);
+          this.props.onRouteChange('home');
+        }
+      });
   }
 }
 
