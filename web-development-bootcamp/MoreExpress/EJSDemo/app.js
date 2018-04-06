15e8373d69448908218a99f8b9c2e97a0e27b627
Danilo Lax <danilolax@live.com>
2018-04-06 13:37:52 +0000

    Added Example for posting requests

--- web-development-bootcamp/MoreExpress/EJSDemo/app.js
+++ web-development-bootcamp/MoreExpress/EJSDemo/app.js
@@ -1,16 +1,31 @@
 const express = require('express');
 const app = express();
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
 
 // "/"    => `Server is running ...`
 app.get(`/`, (req, res) => {
   console.assert(req); // unreferenced parameter
-  res.render('home.ejs');
+  res.render('home');
 });
 
 app.get(`/message/:target`, (req, res) => {
   console.assert(req);
-  let target = req.params.target;
-  res.render('message.ejs', { targetVar: target });
+  const target = req.params.target;
+  res.render('message', { targetVar: target });
+});
+
+app.get(`/posts`, (req, res) => {
+  console.assert(req);
+  const posts = [
+    { title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', author: 'John' },
+    { title: 'Nulla vitae neque rutrum ex rutrum tincidunt.', author: 'Susie' },
+    { title: 'Fusce et justo imperdiet lacus efficitur vestibulum.', author: 'Bob' },
+    { title: 'Mauris vitae neque sed magna congue cursus non.', author: 'Jane' },
+    { title: 'Nunc in dui quis leo facilisis interdum.', author: 'Anna' },
+    { title: 'Morbi sit amet metus consectetur, pulvinar massa quis, efficitur lacus.', author: 'Mary' },
+  ];
+  res.render('posts', { posts: posts });
 });
 
 
